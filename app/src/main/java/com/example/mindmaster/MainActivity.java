2d911fa7a328a91dba141393107bf24d22b842c7
MylesRC <myles_chan@dlsu.edu.ph>
2019-11-20 10:16:08 +0800

    added code

--- app/src/main/java/com/example/mindmaster/MainActivity.java
+++ app/src/main/java/com/example/mindmaster/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.mindmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        AppConstants.initialization(this.getApplicationContext());
+        getSupportActionBar().hide();
+    }
+
+    public void startGame(View view){
+        //Log.i("ImageButton","clicked");
+//        Intent intent = new Intent(this, GameActivity.class);
+//        startActivity(intent);
+//        finish();
     }
 }
