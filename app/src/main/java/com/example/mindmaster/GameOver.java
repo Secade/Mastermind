506e9c1ef3fa40312b26ea9601c1c1ecc5198c23
MylesRC <myles_chan@dlsu.edu.ph>
2019-11-23 06:47:28 +0800

    did stuff

--- app/src/main/java/com/example/mindmaster/GameOver.java
+++ app/src/main/java/com/example/mindmaster/GameOver.java
@@ -5,12 +5,13 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GameOver extends AppCompatActivity {
 
-    TextView tvPersonalBest;
+    TextView tvPersonalBest, tvScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +22,16 @@ public class GameOver extends AppCompatActivity {
         SharedPreferences pref = getSharedPreferences("MyPref",0);
         int scoreSP = pref.getInt("scoreSP",0);
         SharedPreferences.Editor editor = pref.edit();
-        if(score > scoreSP){
+        if(score < scoreSP){
             scoreSP = score;
             editor.putInt("scoreSP", scoreSP);
             editor.commit();
+            Toast.makeText(getApplicationContext(), "Congratulations! You've beat your personal high score!", Toast.LENGTH_LONG).show();
         }
         tvPersonalBest = findViewById(R.id.tvPersonalBest);
         tvPersonalBest.setText(""+scoreSP);
+        tvScore = findViewById(R.id.tvScore);
+        tvScore.setText(""+score);
     }
 
     public void restart(View view){
